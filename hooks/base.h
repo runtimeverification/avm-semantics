674c71a9c0004814943541d272df02f057629047
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-02-22 05:08:36 +0000

    hooks/base: remove b2048_encode

--- hooks/base.h
+++ hooks/base.h
@@ -9,7 +9,4 @@ typedef std::vector<unsigned char> bytes;
 std::string b32_encode(const bytes& in);
 bytes b32_decode(const std::string& in);
 
-std::vector<uint16_t> b2048_encode(const bytes& in);
-bytes b2048_decode(const std::vector<uint16_t> &in);
-
 #endif
