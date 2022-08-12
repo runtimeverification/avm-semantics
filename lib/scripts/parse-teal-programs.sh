fb30665b65fafc7c57f19de93e2c74d3b1619f62
Georgy Lukyanov <mail@geo2a.info>
2022-08-12 14:11:53 +0200

    Add dedicated env variable for parsing scripts

--- lib/scripts/parse-teal-programs.sh
+++ lib/scripts/parse-teal-programs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kparse --sort TealPrograms --directory "${backend_dir}" --module TEAL-PARSER-SYNTAX $1
+kparse --sort TealPrograms --definition ${KAVM_DEFINITION_DIR} --module TEAL-PARSER-SYNTAX $1
