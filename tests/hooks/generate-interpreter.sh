2d742043639483c5b5e9bf5bbe6607b0f85cc1a6
Georgy Lukyanov <mail@geo2a.info>
2022-09-26 11:41:57 +0200

    Test that hooked hash functions work

--- tests/hooks/generate-interpreter.sh
+++ tests/hooks/generate-interpreter.sh
@@ -2,8 +2,38 @@
 
 set -euo pipefail
 
-HOOK_CPPFILES="$@"
+# The interpreter needs to be linked with KLLVM C++ library
 KLLVM_INCLUDES=$(dirname $(which kompile))/../include/kllvm
+
+# The blockchain-k-plugin declares hooked cryptographic functions, and we need both the K and C++ sources
+PLUGIN_INCLUDES=$(realpath "../../.build/usr/lib/kavm/blockchain-k-plugin/include/")
+PLUGIN_K_INCLUDES=$PLUGIN_INCLUDES/kframework
+PLUGIN_CPP_FILES="${PLUGIN_INCLUDES}/c/plugin_util.cpp ${PLUGIN_INCLUDES}/c/crypto.cpp ${PLUGIN_INCLUDES}/c/blake2.cpp"
+
+# The blockchain-k-plugin hooks need libff: both includes and the compiled shared object
+LIBFF_LIB=$(realpath "../../.build/usr/lib/kavm/libff/lib/")
+LIBFF_INCLUDE=$(realpath "../../.build/usr/lib/kavm/libff/include/")
+
+# The custom hooks for KAVM: we need the C++ sources
+KAVM_HOOKS_INCLUDES=$(realpath "../../.build/usr/lib/kavm/include/c/")
+KAVM_HOOKS_CPP_FILES="${KAVM_HOOKS_INCLUDES}/algorand.cpp ${KAVM_HOOKS_INCLUDES}/base.cpp ${KAVM_HOOKS_INCLUDES}/mnemonic.cpp ${KAVM_HOOKS_INCLUDES}/hooks.cpp"
+
+# Clang needs to link these libraries into the interpreter
+CLANG_FLAGS="-lcryptopp -lsecp256k1 -lff -lcurl -lssl -lcrypto -lprocps"
+
 CURDIR=$(pwd)
-kompile $CURDIR/kavm-hooks-tests.k --syntax-module KAVM-HOOKS-TESTS --backend llvm --hook-namespaces KAVM -ccopt -c -ccopt -o -ccopt partial.o
-llvm-kompile $CURDIR/kavm-hooks-tests-kompiled/partial.o main $HOOK_CPPFILES -o $CURDIR/kavm-hooks-tests-kompiled/interpreter -I"$KLLVM_INCLUDES" -lcrypto
+
+# Build the interpreter object file by kompiling the K modules: the semantics and blockchain-k-plugin
+kompile $CURDIR/kavm-hooks-tests.k --backend llvm \
+        --syntax-module KAVM-HOOKS-TESTS          \
+        --hook-namespaces "KAVM KRYPTO"           \
+        -I $PLUGIN_K_INCLUDES                     \
+        -ccopt -c -ccopt -o -ccopt partial.o
+
+# Compile the C++ code of the blockchain-k-plugin's hooks and the custom hooks and link them into the interpreter
+llvm-kompile $CURDIR/kavm-hooks-tests-kompiled/partial.o main \
+             $PLUGIN_CPP_FILES $KAVM_HOOKS_CPP_FILES          \
+             -I"$KLLVM_INCLUDES"                              \
+             -I"$LIBFF_INCLUDE" -L"$LIBFF_LIB"                \
+             $CLANG_FLAGS                                     \
+             -o $CURDIR/kavm-hooks-tests-kompiled/interpreter
--- tests/hooks/test_hooks.py
+++ tests/hooks/test_hooks.py
@@ -39,3 +39,17 @@ def test_is_address_valid(address_literal: str, expected: bytes) -> None:
 def test_decode_encode_address_string(address_literal: str) -> None:
     c_pgm = f'EncodeAddressBytes(DecodeAddressString("{address_literal}"))'
     assert _krun(c_pgm) == f'"{address_literal}"' + ' ~> .'
+
+
+def test_sha256() -> None:
+    c_pgm = f'Sha256("")'
+    assert _krun(c_pgm) == f'"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"' + ' ~> .'
+
+def test_sha512_256() -> None:
+    c_pgm = f'Sha512_256("")'
+    assert _krun(c_pgm) == f'"c672b8d1ef56ed28ab87c3622c5114069bdd3ad7b8f9737498d0c01ecef0967a"' + ' ~> .'
+
+def test_keccak256() -> None:
+    c_pgm = f'Keccak256("")'
+    assert _krun(c_pgm) == f'"c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"' + ' ~> .'
+
