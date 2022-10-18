313d3d9d5d4ec626087b41a858783967809add4c
Noah Watson <107630091+nwatson22@users.noreply.github.com>
2022-10-18 04:46:51 -0500

    Running avm-simulation tests through pytest (#140)
    * Add pytest fixtures for avm-simulation tests
    
    * Fix filepaths in test and change ignored tests to be skipped in pytest
    
    * Change tests to check new panic code instead of just return code
    
    * Improve spacing
    
    * commit poetry.lock
    
    * Remove testing scenarios through the makefile directly, since they are now run through the pytest tests
    
    * Add pytest parallelization to CI
    
    * commit poetry.lock
    
    * Remove broken stage
    
    * Attempt to fix poetry issue in CI
    
    * Towards Github Workflow-based CI
    
    * Execute scenario tests in GitHub Workflow CI
    
    * Re-organize top-level Makefile
    
    * Control avm simulation tests output
    
    * Set number of threads for pytest-xdist from the workflow file
    
    * Apply code style
    
    * pytest parallelism
    
    Co-authored-by: Georgy Lukyanov <mail@geo2a.info>

--- kavm/src/tests/algod_integration/contracts/test_trivial.py
+++ kavm/src/tests/algod_integration/contracts/test_trivial.py
@@ -1,7 +1,6 @@
 from base64 import b64decode
 from typing import Dict
 
-import pytest
 from algosdk import account
 from algosdk.future import transaction
 from algosdk.future.transaction import ApplicationCallTxn, ApplicationCreateTxn, OnComplete, PaymentTxn
