407c38438acb25abf807500a88b71cc2da2693fe
Georgy Lukyanov <mail@geo2a.info>
2022-11-22 09:31:33 +0100

    Drop msgpack.h from hooks; tidy-up other header includes

--- tests/hooks/generate-interpreter.sh
+++ tests/hooks/generate-interpreter.sh
@@ -3,23 +3,26 @@
 set -euo pipefail
 
 # The interpreter needs to be linked with KLLVM C++ library
-KLLVM_INCLUDES=$(dirname $(which kompile))/../include/kllvm
+KLLVM_INCLUDES="$(realpath ../../.build/usr/lib/kavm/kframework/include/kllvm/)"
 
 # The blockchain-k-plugin declares hooked cryptographic functions, and we need both the K and C++ sources
 PLUGIN_INCLUDES=$(realpath "../../.build/usr/lib/kavm/blockchain-k-plugin/include/")
 PLUGIN_K_INCLUDES=$PLUGIN_INCLUDES/kframework
 PLUGIN_CPP_FILES="${PLUGIN_INCLUDES}/c/plugin_util.cpp ${PLUGIN_INCLUDES}/c/crypto.cpp ${PLUGIN_INCLUDES}/c/blake2.cpp"
 
-# The blockchain-k-plugin hooks need libff: both includes and the compiled shared object
+# The blockchain-k-plugin hooks need libff, crypto++: both includes and the compiled shared object
 LIBFF_LIB=$(realpath "../../.build/usr/lib/kavm/libff/lib/")
 LIBFF_INCLUDE=$(realpath "../../.build/usr/lib/kavm/libff/include/")
+
+CRIPTOPP_LIB=$(realpath "../../.build/usr/lib/kavm/cryptopp/lib/")
+CRIPTOPP_INCLUDE=$(realpath "../../.build/usr/lib/kavm/cryptopp/include/")
 
 # The custom hooks for KAVM: we need the C++ sources
 KAVM_HOOKS_INCLUDES=$(realpath "../../.build/usr/lib/kavm/include/c/")
 KAVM_HOOKS_CPP_FILES="${KAVM_HOOKS_INCLUDES}/algorand.cpp ${KAVM_HOOKS_INCLUDES}/base.cpp ${KAVM_HOOKS_INCLUDES}/mnemonic.cpp ${KAVM_HOOKS_INCLUDES}/hooks.cpp"
 
 # Clang needs to link these libraries into the interpreter
-CLANG_FLAGS="-lcryptopp -lsecp256k1 -lff -lcurl -lssl -lcrypto -lprocps"
+CLANG_FLAGS=" -lcryptopp -lsecp256k1 -lff -lcurl -lssl -lcrypto -lprocps "
 
 CURDIR=$(pwd)
 
@@ -32,8 +35,9 @@ kompile $CURDIR/kavm-hooks-tests.k --backend llvm \
 
 # Compile the C++ code of the blockchain-k-plugin's hooks and the custom hooks and link them into the interpreter
 llvm-kompile $CURDIR/kavm-hooks-tests-kompiled/partial.o main -- \
+             -I"$LIBFF_INCLUDE" -L"$LIBFF_LIB"                   \
+             -I"$CRIPTOPP_INCLUDE" -L"$CRIPTOPP_LIB"             \
              $PLUGIN_CPP_FILES $KAVM_HOOKS_CPP_FILES             \
              -I"$KLLVM_INCLUDES"                                 \
-             -I"$LIBFF_INCLUDE" -L"$LIBFF_LIB"                   \
              $CLANG_FLAGS                                        \
              -o $CURDIR/kavm-hooks-tests-kompiled/interpreter
