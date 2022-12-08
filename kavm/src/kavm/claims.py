499a886ba1b901df4ed9c274c3df0f894065a1a3
Noah Watson <noah@nwatson.xyz>
2022-12-08 16:37:56 -0600

    Fix typing

--- kavm/src/kavm/claims.py
+++ kavm/src/kavm/claims.py
@@ -1,7 +1,7 @@
 import json
 from collections import namedtuple
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from algosdk.abi.contract import Contract
 from algosdk.abi.method import Method
@@ -15,7 +15,7 @@ from pyk.ktool.kprove import KProve
 PrePost = namedtuple('PrePost', ['pre', 'post'])
 
 
-def parse_program(file: Optional[Path]):
+def parse_program(file: Optional[Path]) -> KInner:
     if not (file):
         return KApply(
             label=KLabel(name='int__TEAL-OPCODES_PseudoOpCode_PseudoTUInt64', params=()),
@@ -33,7 +33,7 @@ def parse_program(file: Optional[Path]):
         "--module",
         "TEAL-PARSER-SYNTAX",
     ]
-    kast_command += [file]
+    kast_command += [str(file)]
     result = run_process(kast_command)
     return KInner.from_dict(json.loads(result.stdout)["term"])
 
@@ -48,12 +48,8 @@ class SymbolicApplTxn:
         global_schema: Optional[StateSchema] = None,
         approval_program_path: Optional[Path] = None,
         clear_program_path: Optional[Path] = None,
-        note=None,
-        lease=None,
-        rekey_to=None,
         extra_pages: int = 0,
-        boxes=None,
-    ):
+    ) -> None:
         self.sender = KToken("b\"" + sender + "\"", "Bytes")
         self.index = KToken(str(index), "Int")
         self.on_complete = KToken(str(on_complete), "Int")
@@ -62,12 +58,12 @@ class SymbolicApplTxn:
         self.local_schema = local_schema if local_schema else StateSchema(0, 0)
         self.global_schema = global_schema if global_schema else StateSchema(0, 0)
         self.extra_pages = extra_pages
-        self.app_args = []
+        self.app_args: List[KInner] = []
 
-    def add_app_arg(self, arg: KInner):
+    def add_app_arg(self, arg: KInner) -> None:
         self.app_args.append(arg)
 
-    def generate_tvalue_list(self, tvlist):
+    def generate_tvalue_list(self, tvlist: List[KInner]) -> KInner:
         if len(tvlist) == 1:
             return tvlist[0]
         else:
@@ -79,7 +75,7 @@ class SymbolicApplTxn:
                 ],
             )
 
-    def get_specific_field_pre(self):
+    def get_specific_field_pre(self) -> KInner:
         return KApply(
             "<appCallTxFields>",
             [
@@ -112,7 +108,7 @@ class SymbolicApplTxn:
             ],
         )
 
-    def get_symbolic_config_pre(self):
+    def get_symbolic_config_pre(self) -> KInner:
 
         return KApply(
             "<transaction>",
@@ -157,7 +153,7 @@ class SymbolicApplTxn:
             ],
         )
 
-    def get_symbolic_config_post(self):
+    def get_symbolic_config_post(self) -> KInner:
 
         return KApply(
             "<transaction>",
@@ -217,10 +213,10 @@ class SymbolicApplication:
         self.global_state_schema = global_state_schema
         self.approval_pgm_path = approval_pgm_path
         self.clear_pgm_path = clear_pgm_path
-        self.labels = []
+        self.labels: List[KInner] = []
 
-    def preprocess_kast(self, term: KInner):
-        def hex_token_to_k_string(ht: str):
+    def preprocess_kast(self, term: KInner) -> KInner:
+        def hex_token_to_k_string(ht: str) -> str:
             string = ""
             for i in range(0, len(ht), 2):
                 string += "\\x"
@@ -229,10 +225,11 @@ class SymbolicApplication:
 
         if type(term) is KApply:
             if term.label.name == "method__TEAL-OPCODES_PseudoOpCode_TBytesLiteral":
-                selector = str(Method.from_signature(term.args[0].token[1:-1]).get_selector())[2:-1]
-                return KApply(
-                    "byte__TEAL-OPCODES_PseudoOpCode_TBytesLiteral", KToken("\"" + str(selector) + "\"", "String")
-                )
+                if type(term.args[0]) is KToken:
+                    selector = str(Method.from_signature(term.args[0].token[1:-1]).get_selector())[2:-1]
+                    return KApply(
+                        "byte__TEAL-OPCODES_PseudoOpCode_TBytesLiteral", KToken("\"" + str(selector) + "\"", "String")
+                    )
             if len(term.args) == 0:
                 return term
             else:
@@ -252,7 +249,7 @@ class SymbolicApplication:
             else:
                 return term
 
-    def get_config(self):
+    def get_config(self) -> KInner:
         return KApply(
             "<app>",
             [
@@ -290,21 +287,21 @@ class SymbolicAccount:
     ):
         self.address = KToken("b\"" + address + "\"", "Bytes")
         self.balance = KToken(str(balance), "Int")
-        self.apps_config = []
-        self.apps = []
+        self.apps_config: List[KInner] = []
+        self.apps: List[SymbolicApplication] = []
 
-    def add_app(self, application: SymbolicApplication):
+    def add_app(self, application: SymbolicApplication) -> None:
         self.apps_config.append(application.get_config())
         self.apps.append(application)
 
-    def build_created_apps(self):
+    def build_created_apps(self) -> KInner:
         return build_assoc(
             KApply(".AppCellMap"),
             KLabel("_AppCellMap_"),
             self.apps_config,
         )
 
-    def get_symbolic_config_pre(self):
+    def get_symbolic_config_pre(self) -> KInner:
         return KApply(
             "<account>",
             [
@@ -326,62 +323,62 @@ class SymbolicAccount:
 
 
 class KAVMProof:
-    def __init__(self):
-        self._txns = []
-        self._txn_ids = []
-        self._txns_post = []
-        self._accts = []
-        self._accts_config = []
-        self._accts_post = []
-        self._preconditions = []
-        self._postconditions = []
+    def __init__(self) -> None:
+        self._txns: List[KInner] = []
+        self._txn_ids: List[int] = []
+        self._txns_post: List[KInner] = []
+        self._accts: List[SymbolicAccount] = []
+        self._accts_config: List[KInner] = []
+        self._accts_post: List[KInner] = []
+        self._preconditions: List[KInner] = []
+        self._postconditions: List[KInner] = []
 
-    def add_txn(self, txn: SymbolicApplTxn):
+    def add_txn(self, txn: SymbolicApplTxn) -> None:
         self._txns.append(txn.get_symbolic_config_pre())
 
-    def add_acct(self, acct: SymbolicAccount):
+    def add_acct(self, acct: SymbolicAccount) -> None:
         self._accts_config.append(acct.get_symbolic_config_pre())
         self._accts.append(acct)
 
-    def add_precondition(self, precondition: KInner):
+    def add_precondition(self, precondition: KInner) -> None:
         self._preconditions.append(precondition)
 
-    def build_app_creator_map(self):
+    def build_app_creator_map(self) -> KInner:
         creator_map = []
         for acct in self._accts:
             for app in acct.apps:
                 creator_map.append(KApply("_|->_", [KToken(str(app.app_id), "Int"), acct.address]))
         return build_assoc(KApply(".Map"), KLabel("_Map_"), creator_map)
 
-    def build_transactions(self):
+    def build_transactions(self) -> KInner:
         return build_assoc(
             KApply(".TransactionCellMap"),
             KLabel("_TransactionCellMap_"),
             self._txns,
         )
 
-    def build_accounts(self):
+    def build_accounts(self) -> KInner:
         return build_assoc(
             KToken(".Bag", "AccountCellMap"),
             KLabel("_AccountCellMap_"),
             self._accts_config,
         )
 
-    def build_deque(self):
+    def build_deque(self) -> KInner:
         return build_assoc(
             KApply(".List"),
             KLabel("_List_"),
             [KApply("ListItem", KToken("\"" + str(i) + "\"", "Int")) for i in range(0, len(self._txns))],
         )
 
-    def build_deque_set(self):
+    def build_deque_set(self) -> KInner:
         return build_assoc(
             KApply(".Set"),
             KLabel("_Set_"),
             [KApply("SetItem", KToken("\"" + str(i) + "\"", "Int")) for i in range(0, len(self._txns))],
         )
 
-    def prove(self):
+    def prove(self) -> None:
 
         defn = read_kast_definition("tests/specs/verification-kompiled/parsed.json")
         symbol_table = build_symbol_table(defn)
@@ -544,7 +541,7 @@ class KAVMProof:
             ],
         )
 
-        def remove_duplicates(labels):
+        def remove_duplicates(labels: List) -> List:
             return [*set(labels)]
 
         for account in self._accts:
@@ -566,7 +563,7 @@ class KAVMProof:
             ensures=ensures,
         )
 
-        proof = KProve(definition_dir="tests/specs/verification-kompiled/", use_directory=Path("proofs"))
+        proof = KProve(definition_dir=Path("tests/specs/verification-kompiled/"), use_directory=Path("proofs"))
         result = proof.prove_claim(claim=claim, claim_id="test")
 
         if type(result) is KApply and result.label.name == "#Top":
@@ -578,7 +575,7 @@ class KAVMProof:
             print(pretty_print_kast(result, symbol_table=symbol_table))
 
 
-def int_2_bytes(term: KInner):
+def int_2_bytes(term: KInner) -> KInner:
     return KApply(
         "Int2Bytes",
         [
@@ -624,7 +621,7 @@ class AutoProver:
             proof.prove()
 
 
-def main():
+def main() -> None:
 
     txn = SymbolicApplTxn(
         sender="test",
@@ -632,16 +629,16 @@ def main():
         on_complete=0,
         local_schema=StateSchema(0, 0),
         global_schema=StateSchema(0, 0),
-        approval_program_path="tests/teal-sources/test.teal",
-        clear_program_path="tests/teal-sources/test.teal",
+        approval_program_path=Path("tests/teal-sources/test.teal"),
+        clear_program_path=Path("tests/teal-sources/test.teal"),
     )
 
     app1 = SymbolicApplication(
         app_id=1,
         local_state_schema=StateSchema(0, 0),
         global_state_schema=StateSchema(0, 0),
-        approval_pgm_path="tests/teal-sources/test.teal",
-        clear_pgm_path="tests/teal-sources/test.teal",
+        approval_pgm_path=Path("tests/teal-sources/test.teal"),
+        clear_pgm_path=Path("tests/teal-sources/test.teal"),
     )
 
     account1 = SymbolicAccount(address="acct1_addr", balance=1000000000)
