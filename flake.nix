7283426b8cbc84dfe3f962bc770bc4472d3bf751
devops <devops@runtimeverification.com>
2023-03-28 17:31:39 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     blockchain-k-plugin.url = "github:runtimeverification/blockchain-k-plugin/da31c9f4965ffe0a8efc73985011ae6fc9393b93";
     blockchain-k-plugin.inputs.flake-utils.follows = "k-framework/flake-utils";
     blockchain-k-plugin.inputs.nixpkgs.follows = "k-framework/nixpkgs";
-    pyk.url = "github:runtimeverification/pyk/v0.1.203";
+    pyk.url = "github:runtimeverification/pyk/v0.1.204";
     pyk.inputs.flake-utils.follows = "k-framework/flake-utils";
     pyk.inputs.nixpkgs.follows = "k-framework/nixpkgs";
 
